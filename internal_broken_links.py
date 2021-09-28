a7d46c42c99dd0e78c6502210006f9b77f1be073
rafammpp <rafammpp@gmail.com>
2021-09-28 12:56:54 +0200

    reorder code

--- internal_broken_links.py
+++ internal_broken_links.py
@@ -3,15 +3,15 @@ from scrapy.crawler import CrawlerProcess
 
 
 def get_prefix(url):
-    regex = re.compile(r'(http(s)?://(?P<domain>[^/]+))?/(?P<prefix>\w\w(-\w\w)?)/(?P<path>.*)')
+    regex = re.compile(r'(http(s)?://(?P<domain>[^/]+))?/(?P<prefix>[a-zA-Z-]+)/(?P<path>.*)')
     matches = regex.match(url)
-    return matches.group('prefix') if matches else ''
+    return matches.group('prefix')
 
 
 class BrokenLinksSpider(scrapy.Spider):
     name = 'internal-broken-links'
     allowed_domains = []
-    allowed_prefixes = ['']
+    allowed_prefixes = []
     start_urls = []
 
     def __init__(self, urls, prefixes, *args, **kwargs):
@@ -27,21 +27,14 @@ class BrokenLinksSpider(scrapy.Spider):
             except AttributeError:
                 self.allowed_domains.append(url)
 
-            try:
-                self.allowed_prefixes.append(get_prefix(url))
-            except AttributeError:
-                pass
-
         self.allowed_prefixes += prefixes
         
         logging.info(f'Allowed domains: {self.allowed_domains}')
         logging.info(f'Allowed prefixes: {self.allowed_prefixes}')
 
     def is_allowed(self, url):
-        try:
-            url = url[0]
-        except (IndexError):
-            pass
+        if not (url.startswith('/') or url.startswith('http')):
+            return False
         try:
             return not self.allowed_prefixes or get_prefix(url) in self.allowed_prefixes
         except (TypeError, AttributeError):
@@ -51,9 +44,8 @@ class BrokenLinksSpider(scrapy.Spider):
         if response.status != 200:
             item = {}
             item['url'] = response.url
-            item['prev_page'] = response.meta.get('prev_url')
-            item['prev_link_url'] = response.meta.get('prev_href')
-            item['prev_link_text'] = response.meta.get('prev_link_text')
+            item['link_text'] = response.meta.get('link_text')
+            item['previous_page'] = response.meta.get('prev_url')
             item['status'] = response.status
 
             yield item
@@ -64,14 +56,12 @@ class BrokenLinksSpider(scrapy.Spider):
             lang = 'en'
         
         for link in response.css('a'):
-            href = link.xpath('@href').extract()
-            text = link.xpath('text()').extract()
-
+            href = link.xpath('@href').get()
+            text = link.xpath('text()').get()
             if self.is_allowed(href):
                 yield response.follow(link, self.parse,
                     meta={
-                        'prev_link_text': text,
-                        'prev_href': href,
+                        'link_text': text,
                         'prev_url': response.url,
                     },
                     headers={
@@ -88,7 +78,7 @@ args = parser.parse_args()
 if args.urls:
     process = CrawlerProcess(settings={
         "FEEDS": {
-            f"broken_links_{time.strftime('%Y-%m-%d_%H:%M')}.csv": {"format": "csv"},
+            f"exported_broken_links/broken_links_{time.strftime('%Y-%m-%d_%H%M')}.csv": {"format": "csv"},
         },
         "BOT_NAME": 'website_tester',
         "DEPTH_LIMIT": 0,
@@ -98,8 +88,8 @@ if args.urls:
         "DEFAULT_REQUEST_HEADERS": {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'en',
-            'X-OV2-Bot-Name': 'broken-links-bot',
         },
+        "AUTOTHROTTLE_ENABLED": True,
     })
 
     process.crawl(BrokenLinksSpider, urls=args.urls, prefixes=args.prefixes)
